6db02cdc4db847035fbd8fe43a5d2a581a7d1528
Ben Deen <benjamin.deen@gmail.com>
2021-04-14 11:34:24 -0400

    Added CIFTI functionality by using util.readDataMatrix and writeDataMatrix

--- +fpp/+util/convertTtoZ.m
+++ +fpp/+util/convertTtoZ.m
@@ -3,16 +3,13 @@
 
 function convertTtoZ(inputPath,outputPath,dof)
 
-tMap = fpp.util.mriRead(inputPath);
-tVec = reshape(tMap.vol,[numel(tMap.vol) 1]);
+[tVec,hdr] = fpp.util.readDataMatrix(inputPath);
 
 zVec = zeros(size(tVec));
 zVec(tVec>0) = -icdf('norm',tcdf(-tVec(tVec>0),dof),0,1);
 zVec(tVec<0) = icdf('norm',tcdf(tVec(tVec<0),dof),0,1); % Prevents p-values near 1 from rounding to 1
 
-zMap = tMap;
-zMap.vol = reshape(zVec,size(zMap.vol));
-fpp.util.mriWrite(zMap,outputPath);
+fpp.util.writeDataMatrix(zVec,hdr,outputPath);
 
 if ~isempty(fpp.bids.getMetadata(inputPath))
     fpp.bids.jsonReconstruct(inputPath,outputPath);
