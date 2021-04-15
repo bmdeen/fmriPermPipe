2d49e0f5c4a72ac07c83a02d11e26874c8f2d886
Ben Deen <benjamin.deen@gmail.com>
2021-04-15 19:00:55 -0400

    Typo fix

--- +fpp/+util/deleteImageAndJson.m
+++ +fpp/+util/deleteImageAndJson.m
@@ -8,7 +8,7 @@ function deleteImageAndJson(inputPath)
 fpp.util.system(['rm -rf ' inputPath]);
 inputJsonPath = fpp.bids.jsonPath(inputPath);
 if exist(inputJsonPath,'file')
-    fpp.util.system(['rm -rf ' inputJsonpath]);
+    fpp.util.system(['rm -rf ' inputJsonPath]);
 end
 
 end
