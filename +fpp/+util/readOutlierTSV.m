52c9c09386eb5395975332ace4acf7afad38e1f2
Ben Deen <benjamin.deen@gmail.com>
2021-04-18 18:08:48 -0400

    Added nVols (total volumes) output

--- +fpp/+util/readOutlierTSV.m
+++ +fpp/+util/readOutlierTSV.m
@@ -2,9 +2,9 @@
 % Function to read a set of volume indices from a BIDS-style outliers.tsv
 % file, with delta function regressors at each outlier time point.
 %
-% badVols = fpp.util.readOutlierTSV(outlierPath)
+% [badVols,nVols] = fpp.util.readOutlierTSV(outlierPath)
 
-function badVols = readOutlierTSV(outlierPath)
+function [badVols,nVols] = readOutlierTSV(outlierPath)
 
 badVols = [];
 
@@ -14,6 +14,7 @@ for f=1:length(fields)
     thisData = tsv.(fields{f});
     if isnumeric(thisData) && isvector(thisData) && sum(thisData==1)==1 % If field has delta function value
         badVols = [badVols find(thisData==1)];
+        nVols = length(thisData);
     end
 end
 
