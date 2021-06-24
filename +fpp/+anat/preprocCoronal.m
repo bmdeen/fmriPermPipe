8b8bcaedbc9fb6708070603f9b2af63d5bda3d57
Ben Deen <benjamin.deen@gmail.com>
2021-06-24 13:51:40 -0400

    Working MTL segmentation routine using ASHS

--- +fpp/+anat/preprocCoronal.m
+++ +fpp/+anat/preprocCoronal.m
@@ -60,6 +60,7 @@ subjID = fpp.bids.checkNameValue(inputName,'sub');
 coronal2IndividualXfm = fpp.bids.changeName(preprocT2Path,{'desc','space','res','from','to','mode'},...
     {[],[],[],'nativeCoronal','individual','image'},'xfm','.mat');
 individual2CoronalXfm = fpp.bids.changeName(coronal2IndividualXfm,{'from','to'},{'individual','nativeCoronal'});
+individual2CoronalXfmITK = fpp.bids.changeName(individual2CoronalXfm,'desc','ITKFormat');
 preprocT2InCoronalPath = fpp.bids.changeName(preprocT2Path,{'space','res'},{'nativeCoronal',''});
 preprocT1Path = fpp.bids.changeName(preprocT2Path,[],[],'T1w');
 preprocT1InCoronalPath = fpp.bids.changeName(preprocT1Path,{'space','res'},{'nativeCoronal',''});
@@ -74,9 +75,9 @@ end
 for i=1:length(inputCoronalPaths)
     [~,inputCoronalNames{i},~] = fpp.util.fileParts(inputCoronalPaths{i});
     outputCoronalPaths{i} = [anatPreprocDir '/' fpp.bids.changeName(inputCoronalNames{i},'acq','HighResCoronal','T2w','.nii.gz')];
-    fpp.util.copyImageAndJson(inputCoronalPaths{i},outputCoronalPaths{i},'mri');
-    fpp.bids.jsonChangeValue(outputCoronalPaths{i},{'Description','RawSources'},...
-        {'Raw data copied to derivative directory.',fpp.bids.removeBidsDir(inputCoronalPaths{i})});
+%     fpp.util.copyImageAndJson(inputCoronalPaths{i},outputCoronalPaths{i},'mri');
+%     fpp.bids.jsonChangeValue(outputCoronalPaths{i},{'Description','RawSources'},...
+%         {'Raw data copied to derivative directory.',fpp.bids.removeBidsDir(inputCoronalPaths{i})});
 end
 inputCoronalPathsRaw = outputCoronalPaths;
 
@@ -90,28 +91,28 @@ pathsToDelete = inputCoronalPathsRaw;
 fprintf('%s\n',['Step 1, Register and average anatomicals       - ' inputNameGeneric]);
 inputCoronalPaths = outputCoronalPaths;
 outputCoronalPath = fpp.bids.changeName(inputCoronalPaths{1},{'run','space'},{[],'nativeCoronal'});
-addCmd = '';
-for i=2:length(inputCoronalPaths)
-    outputCoronalPaths{i} = fpp.bids.changeName(inputCoronalPaths{i},'space','nativeCoronal');
-    xfmThisRun2FirstRun = fpp.bids.changeName(inputCoronalPaths{i},{'run','from','to','mode'},...
-    {'',['CoronalIm' fpp.util.numPad(i,2)],'nativeCoronal','image'},'xfm','.mat');
-    fpp.fsl.flirt(inputCoronalPaths{i},inputCoronalPaths{1},xfmThisRun2FirstRun,outputCoronalPaths{i},...
-        'dof',6,'interp','sinc');
-    fpp.bids.jsonChangeValue(outputCoronalPaths{i},{'Description','Sources'},...
-        {'Raw data, registered to first anatomical with sinc interpolation.',...
-        fpp.bids.removeBidsDir(inputCoronalPaths{i})});
-    addCmd = [addCmd '-add ' outputCoronalPaths{i} ' '];
-    pathsToDelete = [pathsToDelete outputCoronalPaths{i} xfmThisRun2FirstRun];
-end
-if length(inputCoronalPaths)>1
-    addCmd = [addCmd '-div ' int2str(length(inputCoronalPaths))];
-    fpp.fsl.maths(inputCoronalPaths{1},addCmd,outputCoronalPath);
-    fpp.bids.jsonChangeValue(outputCoronalPath,{'Description','Sources','RawSources','SpatialRef'},...
-        {'Raw data, averaged across runs.',cellfun(removeBidsDir,outputCoronalPaths,'UniformOutput',false),...
-        cellfun(removeBidsDir,inputCoronalPathsRaw,'UniformOutput',false),removeBidsDir(outputCoronalPath)});
-else
-    fpp.util.copyImageAndJson(inputCoronalPaths{1},outputCoronalPath,'mri');
-end
+% addCmd = '';
+% for i=2:length(inputCoronalPaths)
+%     outputCoronalPaths{i} = fpp.bids.changeName(inputCoronalPaths{i},'space','nativeCoronal');
+%     xfmThisRun2FirstRun = fpp.bids.changeName(inputCoronalPaths{i},{'run','from','to','mode'},...
+%     {'',['CoronalIm' fpp.util.numPad(i,2)],'nativeCoronal','image'},'xfm','.mat');
+%     fpp.fsl.flirt(inputCoronalPaths{i},inputCoronalPaths{1},xfmThisRun2FirstRun,outputCoronalPaths{i},...
+%         'dof',6,'interp','sinc');
+%     fpp.bids.jsonChangeValue(outputCoronalPaths{i},{'Description','Sources'},...
+%         {'Raw data, registered to first anatomical with sinc interpolation.',...
+%         fpp.bids.removeBidsDir(inputCoronalPaths{i})});
+%     addCmd = [addCmd '-add ' outputCoronalPaths{i} ' '];
+%     pathsToDelete = [pathsToDelete outputCoronalPaths{i} xfmThisRun2FirstRun];
+% end
+% if length(inputCoronalPaths)>1
+%     addCmd = [addCmd '-div ' int2str(length(inputCoronalPaths))];
+%     fpp.fsl.maths(inputCoronalPaths{1},addCmd,outputCoronalPath);
+%     fpp.bids.jsonChangeValue(outputCoronalPath,{'Description','Sources','RawSources','SpatialRef'},...
+%         {'Raw data, averaged across runs.',cellfun(removeBidsDir,outputCoronalPaths,'UniformOutput',false),...
+%         cellfun(removeBidsDir,inputCoronalPathsRaw,'UniformOutput',false),removeBidsDir(outputCoronalPath)});
+% else
+%     fpp.util.copyImageAndJson(inputCoronalPaths{1},outputCoronalPath,'mri');
+% end
 
 
 
@@ -119,10 +120,12 @@ end
 %%% STEP 2: Registration to individual space
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 fprintf('%s\n',['Step 2, Register to individual space           - ' inputNameGeneric]);
-fpp.fsl.flirt(outputCoronalPath,preprocT2Path,coronal2IndividualXfm,[],'dof',6);   % Initial FLIRT-based xfm
-fpp.fsl.invertXfm(coronal2IndividualXfm,individual2CoronalXfm);
-fpp.fsl.moveImage(preprocT2Path,outputCoronalPath,preprocT2InCoronalPath,individual2CoronalXfm,'interp','sinc');
-fpp.fsl.moveImage(preprocT1Path,outputCoronalPath,preprocT1InCoronalPath,individual2CoronalXfm,'interp','sinc');
+% fpp.fsl.flirt(outputCoronalPath,preprocT2Path,coronal2IndividualXfm,[],'dof',6);   % Initial FLIRT-based xfm
+% fpp.fsl.invertXfm(coronal2IndividualXfm,individual2CoronalXfm);
+% fpp.fsl.moveImage(preprocT2Path,outputCoronalPath,preprocT2InCoronalPath,individual2CoronalXfm,'interp','sinc');
+% fpp.fsl.moveImage(preprocT1Path,outputCoronalPath,preprocT1InCoronalPath,individual2CoronalXfm,'interp','sinc');
+% fpp.bids.jsonChangeValue(preprocT2InCoronalPath,'Description','Raw data, averaged, in NativeCoronal space.');
+% fpp.bids.jsonChangeValue(preprocT1InCoronalPath,'Description','Raw data, averaged, in NativeCoronal space.');
 
 
 
@@ -130,16 +133,26 @@ fpp.fsl.moveImage(preprocT1Path,outputCoronalPath,preprocT1InCoronalPath,individ
 %%% STEP 3: ASHS-based MTL segmentation
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 if exist(ashsDir,'dir')
+    fprintf('%s\n',['Step 3, Medial temporal lobe segmentation      - ' inputNameGeneric]);
     hemis = {'L','R',''};
-    outputSegDir = fpp.bids.changeName(outputCoronalPath,[],[],'ashs','');
-    outputSegPath = fpp.bids.changeName(outputCoronalPath,'desc','ashs','dseg');    % bilateral
+    hemiPrefix = {'L_','R_',''};
+    outputSegDir = fpp.bids.changeName(outputCoronalPath,'acq','','ashs','');
+    outputSegPath = fpp.bids.changeName(outputCoronalPath,{'desc','acq'},{'ashs',''},'dseg');
+    % Note: ASHS requires native-space T1 image, not MNI-aligned
+    nativeT1Path = fpp.bids.changeName(preprocT1Path,{'desc','res','space'},{'','','nativeT1w'});
     % Perform ASHS segmentation
-    fpp.util.system(['ashs_main.sh -I sub-' subjID ' -a ' ashsDir ' -g ' preprocT1InCoronalPath...
-        ' -f ' outputCoronalPath ' -w ' outputSegDir]);
+    if ~exist(outputSegDir,'dir')       %%% TEMPORARY FOR DEBUGGING
+        fpp.util.system(['$ASHS_ROOT/bin/ashs_main.sh -I sub-' subjID ' -a ' ashsDir...
+            ' -g ' nativeT1Path ' -f ' outputCoronalPath ' -w ' outputSegDir]);
+    end
     % Copy and rename output segmentation
+    tmpPath = fpp.bids.changeName(outputSegPath,'desc','ashsTmpPreprocCoronal20920398542');
     fpp.fsl.maths([outputSegDir '/final/sub-' subjID '_right_lfseg_corr_nogray.nii.gz'],...
-        ['-add 100 -add ' outputSegDir '/final/sub-' subjID '_left_lfseg_corr_nogray.nii.gz'],...
+        '-bin -mul 100',tmpPath);
+    fpp.fsl.maths([outputSegDir '/final/sub-' subjID '_right_lfseg_corr_nogray.nii.gz'],...
+        ['-add ' tmpPath ' -add ' outputSegDir '/final/sub-' subjID '_left_lfseg_corr_nogray.nii.gz'],...
         outputSegPath);
+    fpp.util.deleteImageAndJson(tmpPath);
     fpp.util.system(['cp ' fpp.bids.jsonPath(outputCoronalPath) ' ' fpp.bids.jsonPath(outputSegPath)]);
     fpp.bids.jsonChangeValue(outputSegPath,{'Description','Sources'},{['Medial temporal lobe segmentation file, '...
         'generated by Automatic Segmentation of Hippocampal Subfields.'],removeBidsDir(outputCoronalPath)});
@@ -157,9 +170,9 @@ if exist(ashsDir,'dir')
                     ind = [segInd{i} segInd{i}+100];
             end
             outputMaskPath = fpp.bids.changeName(outputSegPath,'desc',['ashs' hemis{h} segNames{i}],'mask');
-            fpp.util.label2ROI(outputSegPath,segInd{i},outputMaskPath);
+            fpp.util.label2ROI(outputSegPath,ind,outputMaskPath);
             fpp.bids.jsonChangeValue(outputMaskPath,{'Type','Description','Sources'},...
-                {'ROI',[hemis{h} '_' segNames{i} ' mask generated by Automatic Segmentation'...
+                {'ROI',[hemiPrefix{h} segNames{i} ' mask generated by Automatic Segmentation'...
                 ' of Hippocampal Subfields'],outputSegPath});
         end
     end
@@ -169,23 +182,26 @@ end
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%% STEP 4: Move ASHS ROIs to individual space
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-fpp.fsl.moveImage(outputSegPath,preprocT2Path,outputSegIndivPath,coronal2IndividualXfm,'interp','nn');
-% Convert dseg to subregion masks
-for i=1:length(segInd)
-    for h=1:length(hemis)
-        switch h
-            case 1
-                ind = segInd{i};
-            case 2
-                ind = segInd{i}+100;
-            case 3
-                ind = [segInd{i} segInd{i}+100];
+if exist(ashsDir,'dir')
+    fprintf('%s\n',['Step 4, Move MTL segments to individual space  - ' inputNameGeneric]);
+    fpp.fsl.moveImage(outputSegPath,preprocT2Path,outputSegIndivPath,coronal2IndividualXfm,'interp','nn');
+    % Convert dseg to subregion masks
+    for i=1:length(segInd)
+        for h=1:length(hemis)
+            switch h
+                case 1
+                    ind = segInd{i};
+                case 2
+                    ind = segInd{i}+100;
+                case 3
+                    ind = [segInd{i} segInd{i}+100];
+            end
+            outputMaskPath = fpp.bids.changeName(outputSegIndivPath,'desc',['ashs' hemis{h} segNames{i}],'mask');
+            fpp.util.label2ROI(outputSegIndivPath,ind,outputMaskPath);
+            fpp.bids.jsonChangeValue(outputMaskPath,{'Type','Description','Sources'},...
+                {'ROI',[hemis{h} '_' segNames{i} ' mask generated by Automatic Segmentation'...
+                ' of Hippocampal Subfields'],outputSegIndivPath});
         end
-        outputMaskPath = fpp.bids.changeName(outputSegIndivPath,'desc',['ashs' hemis{h} segNames{i}],'mask');
-        fpp.util.label2ROI(outputSegIndivPath,segInd{i},outputMaskPath);
-        fpp.bids.jsonChangeValue(outputMaskPath,{'Type','Description','Sources'},...
-            {'ROI',[hemis{h} '_' segNames{i} ' mask generated by Automatic Segmentation'...
-            ' of Hippocampal Subfields'],outputSegIndivPath});
     end
 end
 
