4ad89ebb8904d69d1f107100301f37d21d3c9560
Ben Deen <benjamin.deen@gmail.com>
2021-08-04 21:34:20 -0400

    Added functionality to use surface-only ROI CIFTI to extract data from surface/volume statistic CIFTI

--- +fpp/+func/roiExtractWorker.m
+++ +fpp/+func/roiExtractWorker.m
@@ -14,7 +14,7 @@ function [psc,condNames] = roiExtractWorker(modelDir,roiPath)
 psc = [];
 
 % Check if ROI input is NIFTI or CIFTI
-[~,~,roiExt] = fpp.util.fileParts(roiPath);
+[~,roiName,roiExt] = fpp.util.fileParts(roiPath);
 if ~ismember(lower(roiExt),{'.nii.gz','.nii','.dscalar.nii'})
     error('roiPath must be a NIFTI or CIFTI dscalar file.');
 end
@@ -26,15 +26,43 @@ end
 % Determine model suffix and condition names
 [~,modelName,~] = fpp.util.fileParts(modelDir);
 suffix = fpp.bids.checkNameValue(modelName,'desc');
+roiDesc = fpp.bids.checkNameValue(roiName,'desc');
 condPath = [modelDir '/' fpp.bids.changeName(modelName,[],[],'conditions','.tsv')];
 condTSV = bids.util.tsvread(condPath);
 condNames = condTSV.cond_names;
+
+% For CIFTI files, if ROI lacks volume and statistical outputs have it,
+% generate a temporary ROI file for mean extraction.
+tmpROI = 0;
+if isCifti
+    pscPath = [modelDir '/' fpp.bids.changeName(modelName,'desc',[suffix condNames{1}],'psc',roiExt)];
+    pscHasVol = fpp.util.checkMRIProperty('HasVol',pscPath);
+    roiHasVol = fpp.util.checkMRIProperty('HasVol',roiPath);
+    if fpp.util.checkMRIProperty('HasVol',pscPath) && ~fpp.util.checkMRIProperty('HasVol',roiPath)
+        tmpVolPath = [modelDir '/' fpp.bids.changeName(roiName,{'den','desc'},...
+            {'',[roiDesc 'tmpRoiExtract230951835']},[],'.nii.gz')];
+        tmpROIPath = [modelDir '/' fpp.bids.changeName(roiName,{'den','desc'},...
+            {'',[roiDesc 'tmpRoiExtract230951835']})];
+        % Extract volume from pscPath, multiply by zero
+        fpp.wb.command('cifti-separate',pscPath,'COLUMN',[],['-volume-all ' tmpVolPath]);
+        fpp.wb.command('cifti-maths','vol*0',tmpVolPath,[],['-var vol ' tmpVolPath]);
+        % Construct ROI with zero volume
+        fpp.wb.command('cifti-create-dense-from-template',pscPath,[],tmpROIPath,...
+            ['-cifti ' roiPath ' -volume-all ' tmpVolPath]);
+        roiPath = tmpROIPath;   % set to tmp ROI path
+        tmpROI = 1;
+    end
+end
 
 % Extract PSC values within ROI
 for c=1:length(condNames)
     pscPath = [modelDir '/' fpp.bids.changeName(modelName,'desc',[suffix condNames{c}],'psc',roiExt)];
     pscVal = fpp.wb.command([imageType '-stats'],pscPath,[],[],['-reduce MEAN -roi ' roiPath]);
     psc(c) = str2num(strtrim(pscVal));
+end
+
+if tmpROI
+    fpp.util.deleteImageAndJson({tmpROIPath,tmpVolPath});
 end
 
 end
