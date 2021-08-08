634836057acad66aff3bd1da943e659d7e633635
Ben Deen <benjamin.deen@gmail.com>
2021-08-08 15:08:13 -0400

    Improved functionality for high t-values and dof. E.g., for dof=5000, can correctly convert t-values up to ~40.

--- +fpp/+util/convertTtoZ.m
+++ +fpp/+util/convertTtoZ.m
@@ -11,6 +11,11 @@ zVec = zeros(size(tVec));
 zVec(tVec>0) = -icdf('norm',tcdf(-double(tVec(tVec>0)),dof),0,1);
 zVec(tVec<0) = icdf('norm',tcdf(double(tVec(tVec<0)),dof),0,1); % Prevents p-values near 1 from rounding to 1
 
+if sum(zVec==Inf)>0
+    warning(['Z-stat map was written with infinite values. Cannot convert such '
+        'high t-statistics to z-statistics given MATLAB precision limits - ' outputPath]);
+end
+
 fpp.util.writeDataMatrix(zVec,hdr,outputPath);
 
 if ~isempty(fpp.bids.getMetadata(inputPath))
