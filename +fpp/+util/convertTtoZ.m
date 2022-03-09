98f2d29fe03460d342721090566362e8ba1640f6
Ben Deen <benjamin.deen@gmail.com>
2022-03-09 16:02:35 -0500

    Remove infinite values from output z-map

--- +fpp/+util/convertTtoZ.m
+++ +fpp/+util/convertTtoZ.m
@@ -11,9 +11,14 @@ zVec = zeros(size(tVec));
 zVec(tVec>0) = -icdf('norm',tcdf(-double(tVec(tVec>0)),dof),0,1);
 zVec(tVec<0) = icdf('norm',tcdf(double(tVec(tVec<0)),dof),0,1); % Prevents p-values near 1 from rounding to 1
 
-if sum(zVec==Inf)>0
-    warning(['Z-stat map was written with infinite values. Cannot convert such '
-        'high t-statistics to z-statistics given MATLAB precision limits - ' outputPath]);
+if sum(zVec==Inf | zVec==-Inf)>0
+%     warning(['Z-stat map was written with infinite values. Cannot convert such '...
+%         'high t-statistics to z-statistics given MATLAB precision limits - ' outputPath]);
+    zVec(zVec==Inf) = max(zVec(zVec~=Inf));
+    zVec(zVec==-Inf) = min(zVec(zVec~=-Inf));
+    warning(['WARNING: Some of the t-statistic values in map ' inputPath ' were too large'...
+        ' to convert to z-statistics given MATLAB precision limits. These values have been'...
+        ' set to the extrema of the remaining z-statistic values.']);
 end
 
 fpp.util.writeDataMatrix(zVec,hdr,outputPath);
